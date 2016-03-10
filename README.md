44d40128d47940ba107c5488c2af36063ee267f7
Gabu Lártiga <gabilartiga@gmail.com>
2016-03-10 16:46:12 -0300

    Beautify and Lint JS + Read.me

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-frontend-nanodegree-arcade-game
+Frontend nanodegree Arcade game
 ===============================
 
-Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
+##How to Run
+Open _index.html_ into your browser
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+##How to play
+###Controls
+Use :arrow_left: :arrow_up: :arrow_down: :arrow_right: to move the player
+###Objectives
+- Cross the street to reach the water
+- Avoid bugs
--- js/app.js
+++ js/app.js
@@ -1,124 +1,186 @@
-// Enemies our player must avoid
-var Enemy = function(_x,_y,_w,_h,_speed) {
-    this.x = _x;
-    this.y = _y;
-    this.width = _w;
-    this.height = _h+25;
-    this.speed = _speed;
-    this.collisionBox = {
-        radius: (this.width/3),
-        x: (this.x+(this.width/2)),
-        y: (this.y+((this.height)/1.8)) }; //collision Box (circle lol)
+//TODO: player selection
+//TODO: implement score
+//TODO: collectables
+//TODO: upgrades; more points for gem, any enemy shield, etc.
+var iniPos = {
+    x: (202),
+    y: (375)
+}; // player initial spawn position
+var canvasSize = {
+    width: 505,
+    height: 606
+}; // canvas size
+var ey = [58, 141, 224]; // y-axis enemies spawn positions
+var score = 0;
+/**
+ * @description Creates a new enemy
+ * @constructor
+ * @param {integer} x - The position in x-axis of the enemy
+ * @param {integer} y - The position in y-axis of the enemy
+ * @param {integer} w - The width of the enemy
+ * @param {integer} h - The height of the enemy
+ * @param {integer} speed - The speed of the enemy
+ */
+var Enemy = function(x, y, w, h, speed) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h + 25;
+    this.speed = speed;
+    this.collisionBox = { // collision Box (circle lol)
+        radius: (this.width / 3),
+        x: (this.x + (this.width / 2)),
+        y: (this.y + ((this.height) / 1.8))
+    };
     this.sprite = 'images/enemy-bug.png';
 };
-Enemy.prototype.attachCollisionBox = function(_x){
-    this.collisionBox.x = _x+(this.width/2);
-};
 Enemy.prototype.update = function(dt) {
-    this.x = this.x + this.speed*dt;
+    this.x = this.x + this.speed * dt;
     this.attachCollisionBox(this.x);
+    checkCollisions(allEnemies, player);
+    deleteEnemy(allEnemies); // the array never will be so big, then the loop will never take too much time
 };
-
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    // ctx.beginPath();
-    // ctx.arc(this.collisionBox.x, this.collisionBox.y, this.collisionBox.radius,0,2*Math.PI);
-    // ctx.stroke();
+    // drawCollisionBox(this);
+};
+Enemy.prototype.attachCollisionBox = function(x) {
+    this.collisionBox.x = x + (this.width / 2);
 };
 
-//Player Constructor
-var Player = function(_x, _y, _w, _h){ // Player constructor
-    this.x = _x;
-    this.y = _y;
-    this.width = _w;
-    this.height = _h+25;
+/**
+ * @description Creates a new player
+ * @constructor
+ * @param {integer} x - The position in x-axis of the player
+ * @param {integer} y - The position in y-axis of the player
+ * @param {integer} w - The width of the player
+ * @param {integer} h - The height of the player
+ */
+var Player = function(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.width = w;
+    this.height = h + 25;
     this.collisionBox = { //collision Box (circle lol)
-        radius: (this.width/4.5),
-        x: (this.x+((this.width)/2)),
-        y: (this.y+(this.height/1.75))
+        radius: (this.width / 4.5),
+        x: (this.x + ((this.width) / 2)),
+        y: (this.y + (this.height / 1.75))
     };
     this.sprite = 'images/char-boy.png';
+    this.win = false;
 };
+Player.prototype.update = function(dt) {
+    //if player 'collides' with water
+    if (this.y <= 0) {
+        this.win = true;
+        allEnemies.splice(0, allEnemies.length);
+        window.setTimeout(function() {
+            player.reset();
+        }, 1000);
 
-//player initial position
-var iniPos = {
-    x : (101*2),
-    y : (75*5)
+    } else { // if player is out of water the game start again
+        this.win = false;
+    }
+    this.attachCollisionBox(this.x, this.y);
 };
-Player.prototype.attachCollisionBox = function(_x, _y){
-    this.collisionBox.x = _x+(this.width/2);
-    this.collisionBox.y = _y+(this.height/1.75);
-};
-Player.prototype.update = function(dt){
-    //if player "collides" with water
-    if(this.y<=0){
-        ctx.drawText
-        var setPos = setTimeout(
-            function(){
-                player.x = iniPos.x;
-                player.y = iniPos.y;
-            }
-            , 1000);
-    };
-    this.attachCollisionBox(this.x,this.y);
-    checkCollisions(allEnemies, player);
-};
-Player.prototype.render = function(){
+Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    // ctx.beginPath();
-    // ctx.arc(this.collisionBox.x, this.collisionBox.y, this.collisionBox.radius,0,2*Math.PI);
-    // ctx.stroke();
+    if (this.win) {
+        win();
+    }
+    // drawCollisionBox(this);
 };
-
-Player.prototype.handleInput = function(key){
-    if(this.x<101){ // 'Out of Canvas' sanity check
-    }else{
-        if(key==='left'){this.x = this.x - 101;}; // moving Left
-    };
-    if(this.x>303){ // 'Out of Canvas' sanity check
-    }else{
-        if(key==='right'){this.x = this.x + 101;}; // moving Right
-    };
-
-    if(this.y<=0){ // 'Out of Canvas' sanity check
-    }else{
-        if(key==='up'){this.y = this.y - 83;}; // moving Up
-    };
-    if(this.y>=375){ // 'Out of Canvas' sanity check
-    }else{
-        if(key==='down'){this.y = this.y + 83;}; // moving Down
-    };
+Player.prototype.handleInput = function(key) {
+    if (this.x >= 101 && key === 'left') { // Sanity check
+        this.x = this.x - 101; // move Left
+    }
+    if (this.x <= 303 && key === 'right') {
+        this.x = this.x + 101; // move Right
+    }
+    if (this.y > 0 && key === 'up') {
+        this.y = this.y - 83; // move Up
+    }
+    if (this.y < 375 && key === 'down') {
+        this.y = this.y + 83; // move Down
+    }
 };
+Player.prototype.attachCollisionBox = function(x, y) {
+    this.collisionBox.x = x + (this.width / 2);
+    this.collisionBox.y = y + (this.height / 1.75);
+};
+Player.prototype.reset = function() {
+    this.x = iniPos.x;
+    this.y = iniPos.y;
+};
+var player = new Player(iniPos.x, iniPos.y, 101, 173); //Player instantiation
 
-var ey = [58, 141, 224]; // y-axis enemies spawn positions (-25 ~ 0y-axis)
 var allEnemies = [];
-var enemyCreation = setInterval(createEnemy,1000);
-function createEnemy(){
-    var indy = Math.floor((Math.random()*3)); //random y-axis spawn position
-    var randSpeed = Math.floor((Math.random()*250)+150);
+var enemySpawn = 1000; //time to spawn an enemy: 1sec
+
+//New Enemy instantiation every 1 sec
+window.setInterval(createEnemy, enemySpawn);
+/**
+ * @description Instantiate Enemy in a random position with random speed and add it to allEnemies array
+ */
+function createEnemy() {
+    var indy = Math.floor((Math.random() * 3)); //random y-axis spawn position
+    var randSpeed = Math.floor((Math.random() * 250) + 150);
     var enemy = new Enemy(-101, ey[indy], 101, 173, randSpeed);
     allEnemies.push(enemy);
-};
+}
+/**
+ * @description Delete all the enemies that are outside of the canvas
+ * @param {array} enemies - All the enemies in the canvas
+ */
+function deleteEnemy(enemies) {
+    var enemy;
+    for (var i = 0; i < enemies.length; i++) {
+        enemy = enemies[i];
+        if (enemy.x > 550) {
+            enemies.splice(i, 1);
+        }
+    }
+}
+/**
+ * @description Draws the collision box of an object
+ * @param {object} obj - An object that has collisionBox to draw
+ */
+function drawCollisionBox(obj) {
+    ctx.beginPath();
+    ctx.arc(obj.collisionBox.x, obj.collisionBox.y, obj.collisionBox.radius, 0, 2 * Math.PI);
+    ctx.stroke();
+}
 
-function checkCollisions(enemies, player){  //they are actually circles not boxes but collision Box is the standard
-
-    for(var i=0; i < enemies.length; i++){
+/**
+ * @description Check for collisions using Circle Collision detection
+ * @param {array} enemies - All the enemies in the canvas
+ * @param {object} player - The player instance
+ */
+function checkCollisions(enemies, player) { //they are actually circles not boxes, but collisionBox is the standard name
+    for (var i = 0; i < enemies.length; i++) {
         var enemy = enemies[i];
         var dx = player.collisionBox.x - enemy.collisionBox.x;
         var dy = player.collisionBox.y - enemy.collisionBox.y;
         var distance = Math.sqrt(dx * dx + dy * dy);
         if (distance < player.collisionBox.radius + enemy.collisionBox.radius) {
             enemies.splice(i, 1);
-            player.x=iniPos.x;
-            player.y=iniPos.y;
-        };
-    };
-};
-
-//TODO: delete unused(out of canvas) enemies D:!!!
-
-
-var player = new Player(iniPos.x, iniPos.y,101,173);
+            player.reset();
+            player.win = false;
+        }
+    }
+}
+/**
+ * @description Set and write win text
+ */
+function win() {
+    ctx.font = '36pt Impact';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'white';
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 3;
+    ctx.fillText('Winner!', canvasSize.width / 2, canvasSize.height / 2);
+    ctx.strokeText('Winner!', canvasSize.width / 2, canvasSize.height / 2);
+}
 
 // This listens for key presses and sends the keys to Player.handleInput() method
 document.addEventListener('keyup', function(e) {
@@ -128,6 +190,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
